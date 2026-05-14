d85dad63df6d19590727b13ae84a258de74148a7
Kfay <Kfayyyyy@gmail.com>
2026-05-14 18:15:12 +0200

    Update google-ai-studio-optimizer.user.js

--- google-ai-studio-optimizer.user.js
+++ google-ai-studio-optimizer.user.js
@@ -5,7 +5,8 @@
 // @description  Collapse code + reduce lag + restore messages with stable UI
 // @match        https://aistudio.google.com/*
 // @author       Kfayyy
-// @downloadURL  https://raw.githubusercontent.com/Kfayyy/Google-AI-Studio-Optimizer/main/google-ai-studio-optimizer.user.js
+// @downloadURL https://raw.githubusercontent.com/Kfayyy/Google-AI-Studio-Optimizer/main/google-ai-studio-optimizer.user.js
+// @updateURL https://raw.githubusercontent.com/Kfayyy/Google-AI-Studio-Optimizer/main/google-ai-studio-optimizer.user.js
 // @grant        none
 // ==/UserScript==
 
